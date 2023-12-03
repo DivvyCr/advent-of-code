b74a6f629ba1f3f249aa46528b46c2734a4dc16f
Divvy C <divvycr@gmail.com>
2023-12-03 12:05:50 +0000

    Day 3: Part 2

--- Day-3/main.cpp
+++ Day-3/main.cpp
@@ -1,19 +1,24 @@
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <regex>
 #include <stdio.h>
 #include <string>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
+std::map<std::pair<int, int>, std::vector<int>> gears;
+
 int main() {
   ifstream input("input.txt");
-  long sum_total = 0;
-  long sum_not_part = 0;
+  long sum = 0;
 
   regex rgx("([0-9]+)");
   regex rgx_symb(R"([^\d\.])");
 
+  int cur_idx = 0;
   string prev_line;
   string cur_line;
   string next_line;
@@ -26,48 +31,48 @@ int main() {
     while (regex_search(rgx_start, cur_line.cend(), rgx_match, rgx)) {
       int num_start = std::distance(cur_line.cbegin(), rgx_start) + rgx_match.position(1);
       int num_end = num_start + rgx_match.length();
-
       long num = stol(cur_line.substr(num_start, rgx_match.length()));
-      sum_total += num;
 
-      bool is_part = false;
-
-      if (num_start-1 >= 0 && regex_match(cur_line.substr(num_start-1, 1), rgx_symb)) {
-        is_part = true;
+      if (num_start-1 >= 0 && cur_line[num_start-1] == '*') {
+        /* cout << num_start-1 << ", " << cur_idx << "\n"; */
+        gears[std::make_pair(num_start-1, cur_idx)].push_back(num);
       }
 
-      if (num_end < cur_line.size() && regex_match(cur_line.substr(num_end, 1), rgx_symb)) {
-        is_part = true;
+      if (num_end < cur_line.size() && cur_line[num_end] == '*') {
+        /* cout << num_end << ", " << cur_idx << "\n"; */
+        gears[std::make_pair(num_end, cur_idx)].push_back(num);
       }
 
       if (!prev_line.empty()) {
         for (auto i = num_start-1; i <= num_end; i++) {
-          if (i-1 >= 0 && i < prev_line.size() && regex_match(prev_line.substr(i, 1), rgx_symb)) {
-            is_part = true;
-            break;
-          }
-        }
-      }
-      
-      if (!next_line.empty()) {
-        for (auto i = num_start-1; i <= num_end; i++) {
-          if (i-1 >= 0 && i < next_line.size() && regex_match(next_line.substr(i, 1), rgx_symb)) {
-            is_part = true;
-            break;
+          if (i >= 0 && i < prev_line.size() && prev_line[i] == '*') {
+            /* cout << i << ", " << cur_idx-1 << "\n"; */
+            gears[std::make_pair(i, cur_idx-1)].push_back(num);
           }
         }
       }
 
-      if (!is_part) sum_not_part += num;
+      if (!next_line.empty()) {
+        for (auto i = num_start-1; i <= num_end; i++) {
+          if (i >= 0 && i < next_line.size() && next_line[i] == '*') {
+            /* cout << i << ", " << cur_idx+1 << "\n"; */
+            gears[std::make_pair(i, cur_idx+1)].push_back(num);
+          }
+        }
+      }
 
       rgx_start = rgx_match.suffix().first;
     }
 
     prev_line = cur_line;
     cur_line = next_line;
+    cur_idx++;
   }
 
-  cout << sum_total - sum_not_part << "\n";
+  for (auto &gear : gears) {
+    if (gear.second.size() == 2) sum += gear.second[0]*gear.second[1];
+  }
+  cout << sum << "\n";
 
   return 0;
 }
