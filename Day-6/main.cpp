d6f2fd218d30d1263ba49d06323081d4a226bc66
Divvy C <divvycr@gmail.com>
2023-12-06 09:22:25 +0000

    Day 6: Part 2

--- Day-6/main.cpp
+++ Day-6/main.cpp
@@ -1,42 +1,27 @@
+#include <cmath>
 #include <iostream>
 #include <sstream>
 #include <string>
 
 using namespace std;
 
-// g++ -std=c++17 -g -Wall main.cpp -o out 
-// ./out < input
-
 void solve() {
-  string times;
-  string distances;
-  getline(cin, times);
-  getline(cin, distances);
-  
-  string dump;
-  istringstream itime(times);
-  istringstream idist(distances);
-  itime >> dump;
-  idist >> dump;
+  long long t = 44826981;
+  long long d = 202107611381458;
+  long long result;
 
-  long long answer = 1;
-  long t, d, result;
-  for (long i = 0; i < 4; i++) {
-    itime >> t;
-    idist >> d;
-    long count = 0;
-    for (long j = 1; j < t; j++) {
-      result = j*(t-j);
-      if (result > d) count++;
+  long long count = 0;
+  for (long long i = 0; i < t; i++) {
+    result = i*(t-i);
+    if (result > d) {
+      count++;
     }
-    std::cout << count << std::endl;
-    answer *= count;
   }
-  std::cout << answer << std::endl;
+
+  std::cout << count << std::endl;
 }
 
 int main(int argc, char* argv[]) {
   solve();
-
   return 0;    
 }
